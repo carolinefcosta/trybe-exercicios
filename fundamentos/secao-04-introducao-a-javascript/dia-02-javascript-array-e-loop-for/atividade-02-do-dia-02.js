e321ba2af36a06a3587d9bafe4f06f35fa8c3475
Caroline <carolinefernandesc@hotmail.com>
2022-10-21 20:02:42 -0300

    realizando exercicios

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/atividade-02-do-dia-02.js
@@ -0,0 +1,21 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+/*console.log(numbers);
+
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index++) {
+    soma = soma + numbers[index];
+}
+console.log(soma);
+
+let media = soma / numbers.length;
+console.log(media);
+
+if (media>=20){
+        console.log("valor maior que 20");
+} else {
+        console.log("valor menor que 20");
+}*/
+
