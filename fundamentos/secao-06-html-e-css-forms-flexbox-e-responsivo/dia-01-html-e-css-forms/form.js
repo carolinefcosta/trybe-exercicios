f7d0fd959abfa6e71d5a6d79825fc84cdb0ebfa5
Caroline <carolinefernandesc@hotmail.com>
2022-11-09 08:08:16 -0300

    atividade do dia finalizada

--- fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/form.js
+++ fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/form.js
@@ -4,6 +4,7 @@
 //     evento.preventDefault();
 //   });
 // }
+
 function habilitaBotao () {
   const botaoSubmit = document.getElementById('botaoSubmit');
   const aceitoNovamente = document.getElementById('aceitoNovamente');
@@ -11,5 +12,20 @@ function habilitaBotao () {
 }
 window.onload = function (){
   const aceitoNovamente = document.getElementById('aceitoNovamente');
-  aceitoNovamente.addEventListener('change',habilitaBotao)
+  aceitoNovamente.addEventListener('change',habilitaBotao);
+  const botaoSubmit = document.getElementById("botaoSubmit");
+  botaoSubmit.addEventListener('click', dadosValidosOuInvalidos);
+}
+
+function dadosValidosOuInvalidos () {
+  const nomeCompleto = document.getElementById('nomeCompleto').value.length;
+  const email = document.getElementById('e-mail').value.length;
+
+  if (nomeCompleto < 10 || nomeCompleto > 50) {
+    alert('Dados Inválidos');
+  } else if (email < 10 || email > 50){
+    alert('Dados Inválidos');
+  } else {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip!');
+  }
 }
