cdffa312a8a575d3af08d6961f7900eaa1a81ef4
Caroline <carolinefernandesc@hotmail.com>
2022-11-29 15:17:04 -0300

    atividade do dia

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/script.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/script.js
@@ -1,8 +1,97 @@
-const emailListInData = [
-  'roberta@email.com',
-  'paulo@email.com',
-  'anaroberta@email.com',
-  'fabiano@email.com',
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
 ];
 
-// Adicione seu código aqui
+// Adicione o código do exercício aqui:
+// Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+const authorBornIn1947 = () => {
+  return books.find((book) => book.author.birthYear === 1947).author.name;
+};
+
+// Retorne o nome do livro com menor número de caracteres (menor nome).
+const smallerName = () => {
+  let nameBook;
+
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  });
+  return nameBook;
+}
+
+// Encontre o primeiro livro cujo nome possua 26 caracteres.
+const getNamedBook = () => {
+  return books.find((book) => book.name.length === 26);
+};
+
+// Faça uma função que retorne true, se todas as pessoas autoras nasceram no século XX, ou false, caso contrário.
+const everyoneWasBornOnSecXX = () => {
+  return books.every((book) => (
+    book.author.birthYear >= 1901 && book.author.birthYear <= 2000
+  ));
+};
+
+// Faça uma função que retorne true, se algum livro tiver sido lançado na década de 80, e false, caso contrário.
+const someBookWasReleaseOnThe80s = () => {
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989);
+};
