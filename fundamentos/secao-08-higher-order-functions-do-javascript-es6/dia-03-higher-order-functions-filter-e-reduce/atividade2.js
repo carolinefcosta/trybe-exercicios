1c9ea13ec652cce0cb6ac89b0a53c59c7282587f
Caroline <carolinefernandesc@hotmail.com>
2022-12-01 18:20:33 -0300

    atividade 02 finalizada

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/atividade2.js
@@ -0,0 +1,94 @@
+const countries = [
+  {
+    name: 'Afghanistan',
+    region: 'Asia',
+    currencies: [{ code: 'AFN', name: 'Afghan afghani' }],
+    capital: 'Kabul',
+    population: 40218234,
+    area: 652230
+  },
+  {
+    name: 'Åland Islands',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Mariehamn',
+    population: 28875,
+    area: 1580
+  },
+  {
+    name: 'Albania',
+    region: 'Europe',
+    currencies: [{ code: 'ALL', name: 'Albanian lek' }],
+    capital: 'Tirana',
+    population: 2837743,
+    area: 28748
+  },
+  {
+    name: 'Algeria',
+    region: 'Africa',
+    currencies: [{ code: 'DZD', name: 'Algerian dinar' }],
+    capital: 'Algiers',
+    population: 44700000,
+    area: 2381741
+  },
+  {
+    name: 'American Samoa',
+    region: 'Oceania',
+    currencies: [{ code: 'USD', name: 'United States Dollar' }],
+    capital: 'Pago Pago',
+    population: 55197,
+    area: 199
+  },
+  {
+    name: 'Andorra',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Andorra la Vella',
+    population: 77265,
+    area: 468
+  },
+  {
+    name: 'Angola',
+    region: 'Africa',
+    currencies: [{ code: 'AOA', name: 'Angolan kwanza' }],
+    capital: 'Luanda',
+    population: 32866268,
+    area: 1246700
+  },
+  {
+    name: 'Anguilla',
+    region: 'Americas',
+    currencies: [{ code: 'XCD', name: 'East Caribbean dollar' }],
+    capital: 'The Valley',
+    population: 13452,
+    area: 91
+  }
+];
+
+// 1 - Calcule a quantidade total da população de todos os países.
+const calculaPopulacaoTotal = countries.reduce((somaPaises, paisAtual) => somaPaises + paisAtual.population, 0);
+
+// console.log(calculaPopulacaoTotal);
+
+// 2 - Calcule a área total de todos os países.
+const areaTotalPaises = countries.reduce((areaPaises, areaPaisAtual) => areaPaises + areaPaisAtual.area, 0);
+
+// console.log(areaTotalPaises);
+
+// 🚀 3 - Encontre o país com o maior nome.
+const paisMaiorNome = countries.reduce((maiorNome, nomeAtual) => maiorNome.name.length > nomeAtual.name.length ? maiorNome : nomeAtual);
+
+// console.log(paisMaiorNome);
+
+// 🚀 4 - Retorne a quantidade de vezes que a letra a maiúscula ou minúscula aparece no array de nomes.
+const nomes = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+const transfArray = nomes.join('').toLowerCase().split('');
+
+const contaLetraA = () => transfArray.reduce((acc, curr) => curr === 'a' ? acc += 1 : acc, 0);
+
+// console.log(contaLetraA(nomes));
