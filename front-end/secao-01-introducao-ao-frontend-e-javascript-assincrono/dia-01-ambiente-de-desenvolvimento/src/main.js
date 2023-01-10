1cb4bff2b0be572564006d346ac35e2caf7320a6
Caroline <carolinefernandesc@hotmail.com>
2023-01-10 19:21:45 -0300

    atividade 01-segunda parte finalizada

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import "./style.css";
+import copy from 'clipboard-copy';;
 
 const button = document.querySelector('button');
 const resultPassword = document.querySelector('h2');
@@ -8,4 +9,10 @@ const senhaAleatoria = () => {
   resultPassword.innerHTML = nanoid();
 };
 
+const copiandoSenha = (evento) => {
+  copy(evento.target.innerHTML);
+  alert('Senha copiada!');
+};
+
 button.addEventListener('click', senhaAleatoria);
+resultPassword.addEventListener('click', copiandoSenha);
