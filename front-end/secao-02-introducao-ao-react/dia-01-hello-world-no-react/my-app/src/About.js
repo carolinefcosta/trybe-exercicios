a9d312eee230905e30f8ebabd373b6ab82665640
Caroline <carolinefernandesc@hotmail.com>
2023-01-20 11:07:26 -0300

    atividade 01 do dia

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/my-app/src/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return(
+      <section>
+        <h1> Carol Fernandes </h1>
+        <p> Tenho 28 anos, sou de Natal/RN </p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>HTML Semântico</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+        </ul>
+      </section>
+    )
+  }
+}
+
+export default About;
