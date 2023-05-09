5579b23908b990498b2f3d7caf19cd8715a62ba2
Caroline <carolinefernandesc@hotmail.com>
2023-05-09 18:28:58 -0300

    exercício

--- back-end/secao-04-introducao-ao-desenvolvimento-web-com-node.js/dia-01-node.js-runtime-assincrono/my-node-project/readFileSync.js
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-node.js/dia-01-node.js-runtime-assincrono/my-node-project/readFileSync.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-const nomeDoArquivo = 'meu-arquivo.txt';
-
-try {
-  const data = fs.readFileSync(nomeDoArquivo, 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error(`Erro ao ler o arquivo: ${err.path}`);
-  console.log(err);
+async function main() {
+  try {
+    await fs.writeFile('./meu-arquivo.txt', 'Meu textão heloooooo');
+    console.log('Arquivo escrito com sucesso!');
+  } catch (err) {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  }
 }
+
+main()
