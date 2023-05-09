63aaeb8906f05b2ada019c08ad7a0dd77b470326
Caroline <carolinefernandesc@hotmail.com>
2023-05-09 14:58:48 -0300

    Iniciando atividade

--- back-end/secao-04-introducao-ao-desenvolvimento-web-com-node.js/dia-01-node.js-runtime-assincrono/hello-world/index.js
+++
@@ -1,7 +0,0 @@
-const readline = require('readline-sync');
-
-const name = readline.question('Qual seu nome? ');
-const age = readline.questionInt('Qual sua idade? ');
-
-console.log('Hello, world!');
-console.log(`Seja bem-vinda, ${name} ${age}`);
