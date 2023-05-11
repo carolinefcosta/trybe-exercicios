104b5e983251c4db27de1ec82334079b71fb0532
Caroline <carolinefernandesc@hotmail.com>
2023-05-11 15:55:13 -0300

    Iniciando atividade do dia

---
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-node.js/dia-03-node.js-testes-de-integracao/atividade-do-dia-01/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/chocolates/total', async (req, res) => {
+  const chocolates = await cacauTrybe.getAllChocolates();
+  res.status(200).json({ totalChocolates: chocolates.length });
+});
+
+module.exports = app;
