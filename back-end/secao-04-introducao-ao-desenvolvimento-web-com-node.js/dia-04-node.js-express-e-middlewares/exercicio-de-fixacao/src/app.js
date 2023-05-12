bbd70d11f190b75f014ab5474c6959fb7a8eef19
Caroline <carolinefernandesc@hotmail.com>
2023-05-12 16:25:44 -0300

    primeiro exercicio finalizado

---
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-node.js/dia-04-node.js-express-e-middlewares/exercicio-de-fixacao/src/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const existingId = require('./middlewares/existingId');
+
+const app = express();
+
+const teams = [
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
+
+app.use(express.json());
+
+app.get('/teams', (req, res) => res.json(teams));
+
+app.get('/teams/:id',existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  res.json(team);
+});
+
+app.post('/teams', (req, res) => {
+  const requiredProperties = ['nome', 'sigla'];
+  if (requiredProperties.every((property) => property in req.body)) {
+    const team = { id: nextId, ...req.body };
+    teams.push(team);
+    nextId += 1;
+    res.status(201).json(team);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.put('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  
+  const index = teams.indexOf(team);
+  const updated = { id, ...req.body };
+  teams.splice(index, 1, updated);
+  res.status(201).json(updated);
+});
+
+app.delete('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
+  res.sendStatus(204);
+});
+
+module.exports = app;
---
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-node.js/dia-04-node.js-express-e-middlewares/exercicio-de-fixacao/src/server.js
@@ -0,0 +1,3 @@
+const app = require('./app');
+
+app.listen('3000', () => console.log('Meu servidor subiu na porta 3000! Uhuuu'));
