648425943bd4fa6a2b649e8df3e3ee67f1a2cc03
Caroline <carolinefernandesc@hotmail.com>
2023-06-02 13:19:09 -0300

    Primeiro exercício de fixação

---
+++ back-end/secao-06-node.js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/seeders/20230602141319-users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('Users',
+    [
+      {
+        fullName: 'Henrique',
+        email: 'henrique@test.com',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        fullName: 'Junior',
+        email: 'junior@test.com',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        fullName: 'Eduardo',
+        email: 'edu@test.com',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        fullName: 'Caroline',
+        email: 'carol@test.com',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        fullName: 'Camila',
+        email: 'camila@test.com',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    ], {}),
+
+    down: async (queryInterface) => queryInterface.bulkDelete('Users', null, {}),
+};
